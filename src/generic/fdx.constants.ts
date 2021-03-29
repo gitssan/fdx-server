7456f1f66a810439f42963658308717c0875ff1e
San Jay Valk <san.jay.valk@gmail.com>
2021-03-29 11:45:21 +0200

    *modified regex password to accept specials "foreign" cars

--- src/generic/fdx.constants.ts
+++ src/generic/fdx.constants.ts
@@ -1,5 +1,5 @@
 export const REGEX_ALPHA_SPACES = /^[a-zA-ZÀ-ÿ-. ]{2,20}$/;
-export const REGEX_ALPHA_DIGITS_SPACES_SPECIAL = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{8,}$/;
+export const REGEX_ALPHA_DIGITS_SPACES_SPECIAL = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9À-ÿ!@#\$%\^\&*\)\(+=._-]{8,}$/;
 // https://emailregex.com/ better than the Angular default
 export const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
